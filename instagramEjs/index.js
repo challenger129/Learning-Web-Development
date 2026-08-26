d23393c42b68838b34afc47dd8c52ec7f2d968ee
challenger129 <vedanta.cs.25@nitj.ac.in>
2026-08-26 17:36:10 +0530

    Add instagram EJS project

---
+++ instagramEjs/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/ig/:username", (req,res) => {
+    let { username } = req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    if(data){
+        res.render("instagram.ejs",{ data });
+    }
+    else{
+        res.render("error.ejs");
+    }
+})
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
